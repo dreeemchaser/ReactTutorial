b9de97d6643b05a552c1d5fafbf249b3552cc67a
Alexander <StevenAlexanderDraai@gmail.com>
2024-03-27 13:35:23 +0200

    Initial commit

---
+++ First React App/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+function App() {
+    return(
+        // Fragment - empty set of brackets
+        <>
+            <Header/> {/*Component*/}
+            <Food/>
+            <Footer/>
+        </>
+    );
+}
+
+export default App
