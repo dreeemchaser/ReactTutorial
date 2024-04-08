bd32b1b57561576f7796ad83d57cd9dfa6c81cbd
Alexander <StevenAlexanderDraai@gmail.com>
2024-04-08 13:13:26 +0200

    confused..

---
+++ React - stopwatch/react-stopwatch-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Stopwatch from "./Stopwatch.jsx";
+
+
+function App() {
+
+
+  return (
+    <>
+        <Stopwatch/>
+        <Stopwatch/>
+        <Stopwatch/>
+    </>
+  )
+}
+
+export default App
